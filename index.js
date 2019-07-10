dddccd0fa87a89202bd72929edb6f80682e2975b
Taylor Watson <taylorwatsonb@gmail.com>
2019-07-10 21:17:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function doToElementsInArray(array, callback) {
 
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array){
-  arrat[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
 
 var animals = ["dogs", "cat", "squirrel"];
